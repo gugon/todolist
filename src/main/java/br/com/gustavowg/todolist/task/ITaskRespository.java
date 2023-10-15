699ae8ba0ffd3c572cdbb438073f2861fa66cf9f
gugon <gugon19@gmail.com>
2023-10-15 11:42:28 -0300

    Versão inicial

---
+++ src/main/java/br/com/gustavowg/todolist/task/ITaskRespository.java
@@ -0,0 +1,13 @@
+package br.com.gustavowg.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface ITaskRespository extends JpaRepository<TaskModel, UUID> {
+
+    List<TaskModel> findByIdUser(UUID idUser);
+    
+}
