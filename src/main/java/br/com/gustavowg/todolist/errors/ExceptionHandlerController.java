ca1b5beabf02d630964540d1196ea8e6860ceee8
gugon <gugon19@gmail.com>
2023-10-15 12:46:14 -0300

    Versão inicial v1

---
+++ src/main/java/br/com/gustavowg/todolist/errors/ExceptionHandlerController.java
@@ -0,0 +1,13 @@
+package br.com.gustavowg.todolist.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMostSpecificCause().getMessage());
+    }
+}
